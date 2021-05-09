7dd60e4b4446add3b88f706fcab07ba8c6547787
yevdyko <yevdyko@users.noreply.github.com>
2021-05-09 20:36:04 +0000

    Apply formatting changes

--- src/index.tsx
+++ src/index.tsx
@@ -29,7 +29,10 @@ class P5Wrapper extends React.Component<IP5WrapperProps, IP5WrapperState> {
     };
   }
 
-  static getDerivedStateFromProps(props: IP5WrapperProps, state: IP5WrapperState) {
+  static getDerivedStateFromProps(
+    props: IP5WrapperProps,
+    state: IP5WrapperState
+  ) {
     if (state.sketch !== props.sketch) {
       const { sketch } = props;
       const canvas = new p5(sketch, state.wrapper);
