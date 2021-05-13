e6b780f749b1c1f3345a453971a815ccdb70bc1b
ghtu435 <83675365+ghtu435@users.noreply.github.com>
2021-05-13 15:16:51 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -25,18 +25,15 @@ class P5Wrapper extends React.Component<IP5WrapperProps, IP5WrapperState> {
       ...props,
       canvas: null,
       // TODO: find a workaround since refs in state are bad practice
-      wrapper: this.wrapper
+      wrapper: this.wrapper,
     };
   }
 
-  static getDerivedStateFromProps(
-    props: IP5WrapperProps,
-    state: IP5WrapperState
-  ) {
+  static getDerivedStateFromProps(props: IP5WrapperProps, state: IP5WrapperState) {
     if (state.sketch !== props.sketch) {
       const { sketch } = props;
       const canvas = new p5(sketch, state.wrapper);
-      state.canvas.remove();
+      state.canvas.remove()
       return { ...state, sketch, canvas };
     }
 
@@ -71,10 +68,7 @@ class P5Wrapper extends React.Component<IP5WrapperProps, IP5WrapperState> {
 
   render() {
     return (
-      <div
-        {...this.state.attributes}
-        ref={(wrapper) => (this.wrapper = wrapper)}
-      >
+      <div {...this.state.attributes} ref={(wrapper) => (this.wrapper = wrapper)}>
         {this.props.children}
       </div>
     );
